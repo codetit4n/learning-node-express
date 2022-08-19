c231a8b23a2251c2c77a94013773829e682b8f89
codeTIT4N <lokeshkumar636@yahoo.com>
2022-08-20 02:35:46 +0530

    update 5

---
+++ 7-async-pattern/app.js
@@ -0,0 +1,32 @@
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result.txt', `Result: ${first}, ${second}`)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err))
