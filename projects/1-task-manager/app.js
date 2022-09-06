2cfaad4889f15a08f2e8ceffe0710c620670711f
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-06 15:18:05 +0530

    update 20

--- projects/1-task-manager/app.js
+++ projects/1-task-manager/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5001;
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 const tasks = require('./routes/tasks')
 app.use(express.static('./public'))
@@ -12,6 +14,13 @@ app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 
 
-app.listen(port, () => {
-    console.log("Server listening on port 5001...");
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server listening on port ${port}...`))
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start()
