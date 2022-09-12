6fdb574dd6ef07b6d19a8cefea75cec642e175b8
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-12 14:13:40 +0530

    update 26

---
+++ projects/2-store-api/models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'product name must be provided']
+    },
+    price: {
+        type: Number,
+        required: [true, 'product price must be provided']
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    // I will make this in such a way that you can only provide certain companies
+    company: {
+        type: String,
+        // enum: ['ikea', 'liddy', 'caressa', 'marcos'] //note
+        enum: {
+            value: ['ikea', 'liddy', 'caressa', 'marcos'],
+            message: '{VALUE} is not supported'
+        }
+    }
+})
+module.exports = mongoose.model('Product', productSchema)
