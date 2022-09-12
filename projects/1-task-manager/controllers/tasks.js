44be71f0cea8f14095e48f8139bdb50adfffe58a
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-12 10:24:15 +0530

    update 25 - small fix

--- projects/1-task-manager/controllers/tasks.js
+++ projects/1-task-manager/controllers/tasks.js
@@ -29,7 +29,7 @@ const updateTask = asyncWrapper(async (req, res) => {
         runValidators: true
     })
     if (!task) {
-        return res.status(404).json({ msg: `No task for id: ${taskID}` })
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -38,7 +38,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params; //taskID is alias for id
     const task = await Task.findOneAndDelete({ _id: taskID })
     if (!task) {
-        return res.status(404).json({ msg: `No task for id: ${taskID}` })
+        return next(createCustomError(`No task with id: ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
