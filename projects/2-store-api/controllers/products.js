00e03efd0c9d116e57b75005ba8ddb0cf9c6484d
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-13 17:03:54 +0530

    update 28 - small fix

--- projects/2-store-api/controllers/products.js
+++ projects/2-store-api/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
     res.status(200).json({ products, nbHuts: products.length })
 }
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === true ? true : false
