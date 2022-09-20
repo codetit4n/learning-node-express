31d21c23505a96563d77b3bdf3f45ead14b7b190
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-20 18:51:59 +0530

    proj4 init

---
+++ projects/4-Jobs-API/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+// connect DB
+// const connect
+
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5001;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
