d66dfc0103aab33c1bae3743e0b6be20e24cdc8a
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-27 14:22:30 +0530

    update 40

--- projects/4-Jobs-API/middleware/error-handler.js
+++ projects/4-Jobs-API/middleware/error-handler.js
@@ -9,9 +9,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map(item => item.message) //for multiple validation errors
+      .join(',')
+    customError.statusCode = 400
+  }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = 400
+  }
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCode = 404
   }
   return res.status(customError.statusCode).json({ msg: customError.msg })
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
