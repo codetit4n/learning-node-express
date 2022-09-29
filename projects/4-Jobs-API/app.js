48b0252739b2e292c8bf00a7b51bbb5af8e24b45
codeTIT4N <lokeshkumar636@yahoo.com>
2022-09-29 15:35:30 +0530

    update 41

--- projects/4-Jobs-API/app.js
+++ projects/4-Jobs-API/app.js
@@ -33,6 +33,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get('/', (req, res) => {
+  res.send('jobs-api')
+})
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
