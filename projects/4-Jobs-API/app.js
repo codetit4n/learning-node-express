933714fc8dfa96c8b7be4a1a65208a0a7689cf30
codeTIT4N <lokeshkumar636@yahoo.com>
2022-10-02 15:00:00 +0530

    update 42 - fin

--- projects/4-Jobs-API/app.js
+++ projects/4-Jobs-API/app.js
@@ -7,6 +7,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 
 const express = require('express');
 const app = express();
@@ -34,8 +40,10 @@ app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send('jobs-api')
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 })
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter)
